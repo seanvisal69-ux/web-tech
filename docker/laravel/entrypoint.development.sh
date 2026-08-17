5c366ac75a612cb6d415f67c2fba8e99325e562d
seanvisal69-ux <seanvisal69@gmail.com>
2026-08-17 22:18:41 +0700

    session4.3 done

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-# install package 
 # composer install
+# wait $!
+# npm install
 # wait $!
 php artisan key:generate
 wait $!
 php artisan migrate
+wait $!
+php artisan storage:link
 wait $!
 exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
